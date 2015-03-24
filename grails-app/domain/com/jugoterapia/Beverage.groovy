2740ad6c7c62ba42df26aea97e182c1bcf042d90
josdem <joseluis.delacruz@gmail.com>
2015-03-24 10:25:25 -0600

    Adding Beverage domain

---
+++ grails-app/domain/com/jugoterapia/Beverage.groovy
@@ -0,0 +1,18 @@
+package com.jugoterapia
+
+class Beverage {
+
+  String name
+  String ingredients
+  String recipe
+
+  static constraints = {
+    name blank:false,size:1..50
+    ingredients blank:false,size:1..255
+    recipe blank:false,size:1..255
+  }
+
+  static mapping = {
+    version false
+  }
+}
