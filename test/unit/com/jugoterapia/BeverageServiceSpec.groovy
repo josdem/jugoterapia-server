edc3ea33e226b78db2010fc3c6907337c99af3c1
josdem <joseluis.delacruz@gmail.com>
2015-03-25 16:47:35 -0600

    Adding test to get beverages given an category

--- test/unit/com/jugoterapia/BeverageServiceSpec.groovy
+++ test/unit/com/jugoterapia/BeverageServiceSpec.groovy
@@ -7,14 +7,28 @@ import spock.lang.Specification
  * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
  */
 @TestFor(BeverageService)
-@Mock(Category)
+@Mock([Category,Beverage])
 class BeverageServiceSpec extends Specification {
 
   void "should list categories"() {
     when:
     service.getCategories()
+
     then:
     Category.list() >> true
+  }
+
+  void "should get beverages by category"(){
+    given:
+    def categoryId = 1    
+    Category mockCategory = Mock()
+
+    when:
+    service.getBeverages(categoryId)
+
+    then:
+    Category.get(categoryId) >> mockCategory
+    Beverage.findByCategory(mockCategory) >> true   
   }
 
 }
