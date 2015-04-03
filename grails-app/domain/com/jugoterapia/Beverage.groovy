dca99bcca71bca1883f9e4f914c4d133e0dd9ac4
josdem <joseluis.delacruz@gmail.com>
2015-04-03 16:57:58 -0600

    Udating from 50 to 100 in lenght

--- grails-app/domain/com/jugoterapia/Beverage.groovy
+++ grails-app/domain/com/jugoterapia/Beverage.groovy
@@ -7,13 +7,15 @@ class Beverage {
   String recipe
 
   static constraints = {
-    name blank:false,size:1..50
+    name blank:false,size:1..100
     ingredients blank:false,size:1..255
     recipe blank:false,size:1..255
   }
 
   static mapping = {
     version false
+    ingredients type:"text"
+    recipe type: "text"
   }
 
   static belongsTo = [category : Category]
